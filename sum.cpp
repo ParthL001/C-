3576f67acea41136ae49f8535ebad2f52dd4af02
ParthL001 <parth.lokhande001@gmail.com>
2025-08-09 17:53:24 +0530

    basic Cpp prog of addition

---
+++ sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "enter the 1st number";
+    cin >> a;
+    cout << "enter the 2nd number";
+    cin >> b;
+    int sum = a+b;
+    cout << "sum of the numbers is : " << sum ;
+
+}
